8148d9c9e6383f6fd2a9640dd804c35c653a696e
Van Anh Le <anhlv.enactusftu@gmail.com>
2023-11-29 11:49:34 +0700

    Add exercise title

--- leetcode/ex2_twoSum_II.ts
+++ leetcode/ex2_twoSum_II.ts
@@ -1,4 +1,22 @@
 /**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+ * and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself
+ *
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ *
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ *
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ */
+
+/**
  * Definition for singly-linked list.
  * class ListNode {
  *     val: number
@@ -10,18 +28,18 @@
  * }
  */
 
-class ListNode {
+class ListNode2 {
     val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
+    next: ListNode2 | null
+    constructor(val?: number, next?: ListNode2 | null) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
     }
 }
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+function addTwoNumbers2(l1: ListNode2 | null, l2: ListNode2 | null): ListNode2 | null {
     let rememberVar = 0;
-    let dummy = new ListNode();
+    let dummy = new ListNode2();
     let dummyHead = dummy;
     let sum = 0;
 
@@ -42,13 +60,13 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
         //calculate sum of each node value and check if it has remembered variable
         rememberVar = (sum >= 10) ? 1 : 0;
         sum = sum >= 10 ? sum - 10 : sum;
-        dummyHead.next = new ListNode(sum);
+        dummyHead.next = new ListNode2(sum);
         dummyHead = dummyHead.next;
     }
 
     //if final node sum >= 10, add node = 1 in the end of SLL
     if (rememberVar === 1) {
-        dummyHead.next = new ListNode(rememberVar);
+        dummyHead.next = new ListNode2(rememberVar);
         dummyHead = dummyHead.next;
     }
 
